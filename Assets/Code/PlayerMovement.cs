5b4f64f2232fe4c38a99f47f1950834cf2718216
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-05-22 23:58:25 +0100

    randomised rooms

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,33 +4,102 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Player")]
     public float rotationSpeed;
     public float maxSpeed;
     public Rigidbody rb;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    bool isOnGround;
 
+    [Header("Stats")]
+    public float currentBulletTimer;
+    public float maxBulletTimer;
+    public float timeSlow;
+    public float jumpForce;
+    public float walkSpeed;
+    public float runSpeed;
+
+    [Header("TP System")]
+    public GameObject[] teleports;
+
+    [Header("Randomisers")]
+    public int roomTP;
+    public int maxRooms;
+    public int lootRarity;
+
+    public Transform LootEntrance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        teleports = GameObject.FindGameObjectsWithTag("Room Spawn");
+
+        foreach (var teleport in teleports) { maxRooms++; }
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   //movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
-
         transform.Translate(movementDirection * maxSpeed * Time.deltaTime, Space.World);
-
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        //bulletTime
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            BulletTime();
+        
+
+        }
+        if (Time.timeScale == timeSlow)
+        {
+            currentBulletTimer += Time.deltaTime;
+        }
+        else if (Time.timeScale == 1 && currentBulletTimer > 0) { currentBulletTimer -= Time.deltaTime; }
+        if (currentBulletTimer > maxBulletTimer)
+        {
+            currentBulletTimer = 0;
+            Time.timeScale = 1;
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            maxSpeed = runSpeed;
+        } else
+        {
+            maxSpeed = walkSpeed;
+        }
+    }
+    
+    private void BulletTime()
+    {
+       
+            if (Time.timeScale == 1) {Time.timeScale = timeSlow; }
+            else {Time.timeScale = 1; }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Loot Door")
+        {
+            transform.position = LootEntrance.position;
+        }
+        if (collision.gameObject.tag == "Room Door")
+        {
+            roomTP = Random.Range(0, maxRooms);
+            transform.position = teleports[roomTP].transform.position;
+        }
     }
 }
